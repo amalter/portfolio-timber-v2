fdd6fe1c622852b4bcbbf587da5a0bec110ed51d
Amber Alter <amber@altersego.net>
2023-11-09 16:22:31 -0800

    add eyeball js

--- src/js/pages/front-page/portfolio.js
+++ src/js/pages/front-page/portfolio.js
@@ -1,3 +1,9 @@
 export function eyeBall() {
-  console.log("hello from eyeball!!! 123456789");
+  const eye = document.querySelector(".eye-left");
+
+  window.addEventListener("mousemove", (evt) => {
+    const x = -(window.innerWidth / 2 - evt.pageX) / 160;
+    const y = -(window.innerHeight / 2 - evt.pageY) / 160;
+    eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
+  });
 }
