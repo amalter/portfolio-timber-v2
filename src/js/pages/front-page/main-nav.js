7115ea746dfb77e20f9a9bfa9c4cb86050d495ed
Amber Alter <amber@altersego.net>
2023-11-13 10:47:10 -0800

    set up basic slide-in functionality

--- src/js/pages/front-page/main-nav.js
+++ src/js/pages/front-page/main-nav.js
@@ -1,3 +1,28 @@
 export function mainNav() {
-  console.log("hello using breakpoint helper using breakpoints array!");
+  // Get nav items
+  const mainNavItems = document.querySelectorAll(".front-page-nav li a");
+  //handle click of nav items
+  mainNavItems.forEach(function (item) {
+    item.addEventListener("click", handleClick);
+  });
+
+  console.log(mainNavItems);
+}
+
+function handleClick(e) {
+  const clickedItemId = e.currentTarget.id;
+
+  const pageSections = document.querySelectorAll(
+    "body.front-page .home-section"
+  );
+  if (clickedItemId === "portfolio" || clickedItemId === "about") {
+    e.preventDefault();
+    pageSections.forEach(function (section) {
+      if (section.id === clickedItemId) {
+        section.classList.toggle("show");
+      } else {
+        section.classList.remove("show");
+      }
+    });
+  }
 }
