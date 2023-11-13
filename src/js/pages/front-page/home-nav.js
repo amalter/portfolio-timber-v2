d093adb29b6bbaa82e52a26f488ad6a5e1ebf665
Amber Alter <amber@altersego.net>
2023-11-13 15:17:36 -0800

    check for hash and fix jump link issue

--- src/js/pages/front-page/home-nav.js
+++ src/js/pages/front-page/home-nav.js
@@ -1,26 +1,41 @@
 export function homeNav() {
   // Get nav items
   const mainNavItems = document.querySelectorAll(".front-page-nav li a");
-  //handle open
-  mainNavItems.forEach(function (item) {
-    item.addEventListener("click", openSection);
-  });
-}
 
-function openSection(e) {
-  const clickedItemId = e.currentTarget.id;
-
+  // get sections
   const pageSections = document.querySelectorAll(
     "body.front-page .home-section"
   );
+
+  // handle open
+  mainNavItems.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      openSection(pageSections, e);
+    });
+  });
+
+  pageSections.forEach(function (section) {
+    if (section.id === "portfolio" || section.id === "about") {
+      //add class show if hash is detected in browser URL
+      if (checkHash() === section.id) {
+        section.classList.toggle("show");
+      } else {
+        section.classList.remove("show");
+      }
+      // add close section button
+      closeSection(section);
+    }
+  });
+}
+
+function openSection(pageSections, e) {
+  const clickedItemId = e.currentTarget.id;
   if (clickedItemId === "portfolio-link" || clickedItemId === "about-link") {
     e.preventDefault();
     const cleanItemId = clickedItemId.replace("-link", ""); // Remove "-link"
     pageSections.forEach(function (section) {
       if (section.id === cleanItemId) {
         section.classList.toggle("show");
-        //handle close
-        closeSection(section);
       } else {
         section.classList.remove("show");
       }
@@ -29,25 +44,41 @@ function openSection(e) {
 }
 
 function closeSection(section) {
-  const backButton = document.createElement("a");
-  const circle = document.createElement("div");
-  const arrowIcon = document.createElement("img");
+  // Check if backButton already exists
+  const existingBackButton = section.querySelector(".back-btn");
+  if (!existingBackButton) {
+    const backButton = document.createElement("a");
+    const circle = document.createElement("div");
+    const arrowIcon = document.createElement("img");
 
-  backButton.classList.add("back-btn");
-  circle.classList.add("circle");
+    backButton.classList.add("back-btn");
+    circle.classList.add("circle");
 
-  arrowIcon.src =
-    "wp-content/themes/portfolio-timber-v2/assets/icons/arrow-right-solid.svg";
-  arrowIcon.alt = "back to main";
-  arrowIcon.classList.add("back-icon");
+    arrowIcon.src =
+      "wp-content/themes/portfolio-timber-v2/assets/icons/arrow-right-solid.svg";
+    arrowIcon.alt = "back to main";
+    arrowIcon.classList.add("back-icon");
 
-  circle.appendChild(arrowIcon);
-  backButton.appendChild(circle);
-  section.appendChild(backButton);
+    circle.appendChild(arrowIcon);
+    backButton.appendChild(circle);
+    section.appendChild(backButton);
 
-  // Add event listener to handle the back action
-  backButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    section.classList.remove("show");
-  });
+    // Add event listener to handle the back action
+    backButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      section.classList.remove("show");
+    });
+  }
+}
+
+function checkHash() {
+  // Get the current URL
+  var currentURL = window.location.href;
+
+  // Check if the URL contains "#portfolio" or "#about"
+  if (currentURL.includes("#portfolio")) {
+    return "portfolio";
+  } else if (currentURL.includes("#about")) {
+    return "about";
+  }
 }
