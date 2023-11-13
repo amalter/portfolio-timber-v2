99dc9a371b73cf7d0e4072c02f4fedcc7791f9e8
Amber Alter <amber@altersego.net>
2023-11-13 11:37:46 -0800

    add close button

--- src/js/pages/front-page/main-nav.js
+++ src/js/pages/front-page/main-nav.js
@@ -1,15 +1,13 @@
 export function mainNav() {
   // Get nav items
   const mainNavItems = document.querySelectorAll(".front-page-nav li a");
-  //handle click of nav items
+  //handle open
   mainNavItems.forEach(function (item) {
-    item.addEventListener("click", handleClick);
+    item.addEventListener("click", openSection);
   });
-
-  console.log(mainNavItems);
 }
 
-function handleClick(e) {
+function openSection(e) {
   const clickedItemId = e.currentTarget.id;
 
   const pageSections = document.querySelectorAll(
@@ -20,9 +18,35 @@ function handleClick(e) {
     pageSections.forEach(function (section) {
       if (section.id === clickedItemId) {
         section.classList.toggle("show");
+        //handle close
+        closeSection(section);
       } else {
         section.classList.remove("show");
       }
     });
   }
+}
+
+function closeSection(section) {
+  const backButton = document.createElement("a");
+  const circle = document.createElement("div");
+  const arrowIcon = document.createElement("img");
+
+  backButton.classList.add("back-btn");
+  circle.classList.add("circle");
+
+  arrowIcon.src =
+    "wp-content/themes/portfolio-timber-v2/assets/icons/arrow-right-solid.svg";
+  arrowIcon.alt = "back to main";
+  arrowIcon.classList.add("back-icon");
+
+  circle.appendChild(arrowIcon);
+  backButton.appendChild(circle);
+  section.appendChild(backButton);
+
+  // Add event listener to handle the back action
+  backButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    section.classList.remove("show");
+  });
 }
