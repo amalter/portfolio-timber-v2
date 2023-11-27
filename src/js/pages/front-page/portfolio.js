41fb26c1db36ef4efb90e322acd318eccab0f82f
Amber Alter <amber@altersego.net>
2023-11-27 14:59:40 -0800

    adjust eyeball tracking

--- src/js/pages/front-page/portfolio.js
+++ src/js/pages/front-page/portfolio.js
@@ -2,8 +2,8 @@ export function eyeBall() {
   const eye = document.querySelector(".eye-left");
 
   window.addEventListener("mousemove", (evt) => {
-    const x = -(window.innerWidth / 2 - evt.pageX) / 160;
-    const y = -(window.innerHeight / 2 - evt.pageY) / 160;
+    const x = -(window.innerWidth / 2 - evt.pageX) / 360;
+    const y = -(window.innerHeight / 2 - evt.pageY) / 360;
     eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
   });
 }
