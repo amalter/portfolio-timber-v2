b5be3fad1e03d2fb00c130fe09bd631f884b6dd1
Amber Alter <amber@altersego.net>
2023-11-30 14:51:41 -0800

    fix js error

--- src/js/pages/front-page/portfolio.js
+++ src/js/pages/front-page/portfolio.js
@@ -1,9 +1,11 @@
 export function eyeBall() {
   const eye = document.querySelector(".eye-left");
 
-  window.addEventListener("mousemove", (evt) => {
-    const x = -(window.innerWidth / 2 - evt.pageX) / 360;
-    const y = -(window.innerHeight / 2 - evt.pageY) / 360;
-    eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
-  });
+  if (eye) {
+    window.addEventListener("mousemove", (evt) => {
+      const x = -(window.innerWidth / 2 - evt.pageX) / 360;
+      const y = -(window.innerHeight / 2 - evt.pageY) / 360;
+      eye.style.transform = `translateY(${y}px) translateX(${x}px)`;
+    });
+  }
 }
