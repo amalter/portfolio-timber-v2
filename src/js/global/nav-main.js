63da043bb162e1a013d778f1894b67794d2fa423
Amber Alter <amber@altersego.net>
2023-12-07 16:06:51 -0800

    add keyboard accessibility for main nav

--- src/js/global/nav-main.js
+++ src/js/global/nav-main.js
@@ -20,9 +20,25 @@ export function navMain() {
     hamIcon.addEventListener("click", toggleNav);
     hamIcon.addEventListener("touchstart", toggleNav);
 
+    // Handle keyboard accessibility for ham-icon
+    hamIcon.addEventListener("keydown", function (event) {
+      if (event.key === "Enter" || event.key === " ") {
+        // Trigger the same action as the click event for keyboard users
+        toggleNav();
+      }
+    });
+
     // Handle click event for close button
     closeBtn.addEventListener("click", function () {
       navMain.style.right = "-100%";
+    });
+
+    // Handle keyboard accessibility for closeBtn
+    closeBtn.addEventListener("keydown", function (event) {
+      if (event.key === "Enter" || event.key === " ") {
+        // Trigger the same action as the click event for keyboard users
+        navMain.style.right = "-100%";
+      }
     });
   }
 }
